8e27a04c48e38a82f411bd6cdc385d6846d80be6
kristo <kristoferjussmann@gmail.com>
2025-11-06 00:46:30 +0200

    Set site URL to GitHub Pages path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://ker102.github.io/Astroblog.github.io/',
 	integrations: [mdx(), sitemap()],
 });
