1c2b5bb42afe08dc43c053473e9ea8328bc4a899
Kristofer Jussmann <kristoferjussmann@gmail.com>
2026-01-14 09:32:32 +0200

    enchance the home ui while keeping it simple and minilaistic, this is not my main portfolio

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'KJ';
+export const SITE_DESCRIPTION = 'Kristofer Jussmann — Technical Blog & Minimalistic Portfolio. AI Engineering, DevOps, Full-Stack Development.';
